67050af4a555fb208ba42f21dd2030ed766fc249
Luiz <frickajr@gmail.com>
2016-07-31 12:14:21 -0300

    ajustes tela inicial

--- Assets/SelectPlayer.cs
+++ Assets/SelectPlayer.cs
@@ -6,12 +6,22 @@ public class SelectPlayer : MonoBehaviour {
     public int playerNumber;
     public Transform ringPosition;
     public GameObject ring;
+    public float delay = 1f;
+    public Animator anim;
+    public string level;
     
     void OnMouseUpAsButton()
     {
         
         Player.numberPlayer = playerNumber;
         Instantiate(ring,ringPosition.position, ringPosition.rotation, transform);
+        anim.SetTrigger("roda");
+        Invoke("loadScene", delay);
         Debug.Log("Click : "+Player.numberPlayer);
+    }
+
+    void loadScene()
+    {
+        Application.LoadLevel(level);
     }
 }
