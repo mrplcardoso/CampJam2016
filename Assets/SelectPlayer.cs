4879b2b7fcc34e5d9a2c9e849360ee3cf1ce3848
Luiz <frickajr@gmail.com>
2016-07-31 10:26:10 -0300

    classe Player.cs

---
+++ Assets/SelectPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectPlayer : MonoBehaviour {
+
+    public int playerNumber;
+    public Transform ringPosition;
+    public GameObject ring;
+
+    void OnMouseUpAsButton()
+    {
+        Player.numberPlayer = playerNumber;
+        Instantiate(ring, ringPosition);
+    }
+}
